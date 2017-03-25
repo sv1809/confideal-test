14952b0b419c4b0f333e6cc4785bca301a715a9c
sv1809 <sv1809@hotmail.com>
2017-03-25 11:29:06 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import { render } from "react-dom";
+
+// import Root from "./root/Root";
+// import configureStore from "./configureStore";
+
+// const store = configureStore();
+
+// render(<Root store={store} />,
+//     document.getElementById("root"));
